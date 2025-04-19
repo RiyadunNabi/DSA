fb4dd951b894418c6b99d3292e97bc4870668ecb
Riyadun Nabi (Riyad) <160701434+RiyadunNabi@users.noreply.github.com>
2025-04-19 22:44:54 +0600

    Update 7_no_of_components.cpp

--- Graph_Series_Striver/7_no_of_components.cpp
+++ Graph_Series_Striver/7_no_of_components.cpp
@@ -2,80 +2,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
-class Solution {
-    private:
-    void dfs(vector<vector<int>>& adjL, vector<bool> &visited, int node){
-        
+class Solution
+{
+private:
+    void dfs(vector<vector<int>> &adjL, vector<bool> &visited, int node)
+    {
+        visited[node] = true;
         for (int i : adjL[node])
         {
-            if(!visited[i]){
-                //visited[i]=true;
+            if (!visited[i])
+            {
                 dfs(adjL, visited, i);
             }
         }
-        
     }
 
-  public:
-    int numProvinces(vector<vector<int>> adj, int n) {
-        vector<vector<int>> adjL(n+1);
+public:
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        int n = isConnected.size();
+        vector<vector<int>> adjL(n + 1);
+        vector<vector<int>> adj(isConnected);
         for (int i = 0; i < n; i++)
         {
-            for (int j = i+1; j<n; j++)
+            for (int j = i + 1; j < n; j++)
             {
-                if(adj[i][j]==1){
-                    adjL[i+1].push_back(j+1);
-                    adjL[j+1].push_back(i+1);
+                if (adj[i][j] == 1)
+                {
+                    adjL[i + 1].push_back(j + 1);
+                    adjL[j + 1].push_back(i + 1);
                 }
-            }  
+            }
         }
 
-        vector<bool> visited(n+1, false);
-        int cnt=0;
+        vector<bool> visited(n + 1, false);
+        int cnt = 0;
         for (int i = 1; i <= n; i++)
         {
-            if(!visited[i]){
+            if (!visited[i])
+            {
                 cnt++;
-                visited[i]=true;
+                // visited[i]=true;
                 dfs(adjL, visited, i);
             }
         }
         return cnt;
-        
     }
 };
 
 //{ Driver Code Starts.
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
-        int V,x;
-        cin>>V;
-        
+    while (t--)
+    {
+        int V, x;
+        cin >> V;
+
         vector<vector<int>> adj;
-        
-        for(int i=0; i<V; i++)
+
+        for (int i = 0; i < V; i++)
         {
             vector<int> temp;
-            for(int j=0; j<V; j++)
+            for (int j = 0; j < V; j++)
             {
-                cin>>x;
+                cin >> x;
                 temp.push_back(x);
             }
             adj.push_back(temp);
         }
-        
 
         Solution ob;
-        cout << ob.numProvinces(adj,V) << endl;
-    
-cout << "~" << "\n";
-}
+        cout << ob.findCircleNum(adj) << endl;
+
+        cout << "~" << "\n";
+    }
     return 0;
 }
