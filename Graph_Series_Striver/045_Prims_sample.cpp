d960e29fa8021d0f5840fcdcb4b3fca960a371ce
Riyadun Nabi (Riyad) <160701434+RiyadunNabi@users.noreply.github.com>
2025-05-17 21:01:42 +0600

    Update and rename 045_Prims_adjMatrix.cpp to 045_Prims_sample.cpp

--- Graph_Series_Striver/045_Prims_sample.cpp
+++ Graph_Series_Striver/045_Prims_sample.cpp
@@ -1,5 +1,66 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+const int MAX = 100;
+
+int primMSTWithPQ(int graph[MAX][MAX], int V) {
+    vector<bool> inMST(V, false);
+    vector<int> key(V, INT_MAX);
+
+    // Min-heap: (weight, vertex)
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    key[0] = 0;
+    pq.push({0, 0}); // Start from vertex 0
+
+    int totalWeight = 0;
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        int weight = pq.top().first;
+        pq.pop();
+
+        if (inMST[u]) continue;
+        inMST[u] = true;
+        totalWeight += weight;
+
+        for (int v = 0; v < V; ++v) {
+            if (graph[u][v] && !inMST[v] && graph[u][v] < key[v]) {
+                key[v] = graph[u][v];
+                pq.push({key[v], v});
+            }
+        }
+    }
+
+    return totalWeight;
+}
+
+int main() {
+    int V;
+    cout << "Enter number of vertices: ";
+    cin >> V;
+
+    int graph[MAX][MAX];
+    cout << "Enter the adjacency matrix:\n";
+    for (int i = 0; i < V; ++i)
+        for (int j = 0; j < V; ++j)
+            cin >> graph[i][j];
+
+    int mstWeight = primMSTWithPQ(graph, V);
+    cout << "Total weight of MST: " << mstWeight << endl;
+
+    return 0;
+}
+
+
+
+
+//AdjMatrix
+/* #include <iostream>
+#include <vector>
 #include <climits>
 using namespace std;
 
@@ -50,3 +111,4 @@ int main() {
 
     return 0;
 }
+*/
